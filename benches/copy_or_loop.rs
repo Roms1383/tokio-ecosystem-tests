51458397660973505b89f8db07740f0c7c7d9bf9
Roms1383 <romain.kelifa@gmail.com>
2022-02-09 14:30:48 +0700

    :white_check_mark: add benchmark for copy vs loop

---
+++ benches/copy_or_loop.rs
@@ -0,0 +1,57 @@
+use bytes::{BufMut, Bytes};
+use tokio::io;
+use tokio_stream::StreamExt;
+use tokio_util::io::StreamReader;
+
+use criterion::async_executor::FuturesExecutor;
+use criterion::*;
+
+async fn copy(
+    stream: tokio_stream::Iter<std::vec::IntoIter<Result<bytes::Bytes, std::io::Error>>>,
+) -> io::Result<Vec<u8>> {
+    let mut reader = StreamReader::new(stream);
+    let mut writer: Vec<u8> = vec![];
+
+    io::copy(&mut reader, &mut writer).await?;
+
+    Ok(writer)
+}
+
+async fn iterate(
+    mut stream: tokio_stream::Iter<std::vec::IntoIter<Result<bytes::Bytes, std::io::Error>>>,
+) -> io::Result<Vec<u8>> {
+    let mut buf = Vec::<u8>::new();
+
+    while let Some(io::Result::Ok(bytes)) = stream.next().await {
+        buf.put(bytes);
+    }
+
+    Ok(buf)
+}
+
+fn bench(c: &mut Criterion) {
+    let mut group = c.benchmark_group("copy_or_loop");
+    group.bench_function("copy", move |b| {
+        b.to_async(FuturesExecutor).iter(|| async {
+            let stream = tokio_stream::iter(vec![
+                io::Result::Ok(Bytes::from_static(&[0, 1, 2, 3])),
+                io::Result::Ok(Bytes::from_static(&[4, 5, 6, 7])),
+                io::Result::Ok(Bytes::from_static(&[8, 9, 10, 11])),
+            ]);
+            copy(stream).await
+        })
+    });
+    group.bench_function("loop", move |b| {
+        b.to_async(FuturesExecutor).iter(|| async {
+            let stream = tokio_stream::iter(vec![
+                io::Result::Ok(Bytes::from_static(&[0, 1, 2, 3])),
+                io::Result::Ok(Bytes::from_static(&[4, 5, 6, 7])),
+                io::Result::Ok(Bytes::from_static(&[8, 9, 10, 11])),
+            ]);
+            iterate(stream).await
+        })
+    });
+}
+
+criterion_group!(benches, bench);
+criterion_main!(benches);
